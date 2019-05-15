fec4639cfa178bc31f2ba54baf800ee55d758cc7
cuongdqpayment <cuongdq.payment@gmail.com>
2019-05-15 16:56:58 +0700

    cuongdq push by sh

--- src/pipes/linkify.ts
+++ src/pipes/linkify.ts
@@ -12,30 +12,34 @@ export class LinkifyPipe implements PipeTransform {
         let valueLinkify = value;
         let links = [];
     
-            //URLs starting with http://, https://, or ftp://    
-            valueLinkify = valueLinkify.replace(/(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim
-                , function (url) {
-                    links.push(url);
-                    return "<a href='"+url+"' target='_blank'>" + url + "</a>";
-                }
-            );
-    
-            //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-            valueLinkify = valueLinkify.replace(/([ ])([\w-]+\.[\S]+(\b|$))/gim
-                , function (url) {
-                    links.push('http://'+url.trim());
-                    return " <a href='http://"+url.trim()+"' target='_blank'>" + url.trim() + "</a>";
-                }
-            );
-    
-            //Change email addresses to mailto:: links.
-            valueLinkify = valueLinkify.replace(/(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim
-                ,
-                function (url) {
-                    links.push('mailto:'+url);
-                    return "<a href='mailto:"+url+"' target='_blank'>" + url + "</a>";
-                }
+            if (valueLinkify){
+                //URLs starting with http://, https://, or ftp://    
+                valueLinkify = valueLinkify.replace(/(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim
+                    , 
+                    function (url) {
+                        links.push(url);
+                        return "<a href='"+url+"' target='_blank'>" + url + "</a>";
+                    }
                 );
+        
+                //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+                valueLinkify = valueLinkify.replace(/([ ])([\w-]+\.[\S]+(\b|$))/gim
+                    , 
+                    function (url) {
+                        links.push('http://'+url.trim());
+                        return " <a href='http://"+url.trim()+"' target='_blank'>" + url.trim() + "</a>";
+                    }
+                );
+        
+                //Change email addresses to mailto:: links.
+                valueLinkify = valueLinkify.replace(/(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim
+                    ,
+                    function (url) {
+                        links.push('mailto:'+url);
+                        return "<a href='mailto:"+url+"' target='_blank'>" + url + "</a>";
+                    }
+                );
+            }
 
         if (isUrl==='links') return links; //array off urls
 
